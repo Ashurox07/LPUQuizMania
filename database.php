227ab34c0500415678f6d4676209bb233ebb7a2a
Ashurox07 <unclebigbay@hashnode.com>
2023-10-17 21:14:54 +0530

    Initial commit

---
+++ database.php
@@ -0,0 +1,2 @@
+<?php
+$con = new mysqli('localhost', 'root', '', 'exam') or die("Could not connect to mysql" . mysqli_error($con));
---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>| Online Quiz System |</title>
+    <link rel="stylesheet" type="text/css" href="css/index.css" />
+    <link rel="shortcut icon" type="image/png" href="image/logo.png" />
+    <style type="text/css">
+        body {
+            width: 100%;
+            background: url(image/book.png);
+            background-position: center center;
+            background-repeat: no-repeat;
+            background-attachment: fixed;
+            background-size: cover;
+        }
+    </style>
+</head>
+
+<body>
+    <center>
+        <div class="intro">
+            <h1> online quiz system </h1>
+            <a href="main.html" class="btn">home</a>&emsp;&emsp;
+            <a href="login.php" class="btn"> login </a> &emsp;&emsp;
+            <a href="register.php" class="btn"> register </a>
+
+            <h2> Good &nbsp;Luck. </h2>
+        </div>
+    </center>
+</body>
+
+</html>
